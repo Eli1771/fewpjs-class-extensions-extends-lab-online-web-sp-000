6d09d1b3179a7ff0e7dfd3ff95cf99e9846d4747
elijahmm17 <elijahmm17@gmail.com>
2019-10-15 16:55:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  
+  get countSides() {
+    return this.sides.length;
+  }
+  
+  get perimeter() {
+    return this.sides.reduce((total, side) => total + side);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides === 3) {
+      console.log('has three sides!');
+      console.log('1: ' + sides[0]);
+      console.log('2: ' + sides[1]);
+      console.log('3: ' + sides[2]);
+    }
+  }
+}
